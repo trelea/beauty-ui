f0dae28c65309fa3922607fd21cdd1568b1d73bd
trelea <treleamarius76@gmail.com>
2024-11-12 00:12:47 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
     base: '/',
     preview: {
-        host: true,
-        port: 2005,
+      host: true,
+      port: parseInt(env.PORT),
     },
     plugins: [react()],
     resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
+    build: {
+      chunkSizeWarningLimit: 1600,
+    },
+  };
 });
