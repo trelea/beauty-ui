14a783fcc146d3a5e11de1dac1fa41238612001d
trelea <treleamarius76@gmail.com>
2024-11-12 21:44:02 +0200

    fix bug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,22 +1,26 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx';
+import { da } from 'date-fns/locale';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 export const toISO8601DateString = (dateObj: Date, dec?: boolean): string => {
-	const data: string[] = dateObj.toLocaleDateString().split("/");
-	let dayBack: string | undefined;
-	if (dec === true) {
-		dayBack = String(Number(data[1]) - 1);
-	}
-	return `${data[2]}-${data[0].length === 1 ? `0${data[0]}` : data[0]}-${dec
-		? dayBack?.length === 1
-			? `0${dayBack}`
-			: dayBack
-		: data[1].length === 1
-			? `0${data[1]}`
-			: data[1]
-		}`;
+  const data: string[] = dateObj.toLocaleDateString('ro-MD').split('.');
+
+  let dayBack: string | undefined;
+  if (dec === true) {
+    dayBack = String(Number(data[0]) - 1);
+  }
+
+  return `${data[2]}-${data[1].length === 1 ? `0${data[1]}` : data[1]}-${
+    dec
+      ? dayBack?.length === 1
+        ? `0${dayBack}`
+        : dayBack
+      : data[0].length === 1
+      ? `0${data[0]}`
+      : data[0]
+  }`;
 };
